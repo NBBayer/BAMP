5f6858301175f98d387eafc3dae77b6265316caf
Your NameChrisS2010 <christian.strauch@protonmail.com>
2022-04-18 10:52:51 +0200

    Creating of new variables, lin. and polynomial models added - ChS, 18.04.22

--- 220416_Basic Modellin.R
+++ 220416_Basic Modellin.R
@@ -7,6 +7,9 @@ summary(Best.Western.Premium.Schwarzwald.1..Halbjahr.2019.Zusammenfassung)
 library(tidyr)
 library(dplyr)
 library(fasttime)
+#install.packages("lubridate")
+library(lubridate)
+library(ggplot2)
 
 bw_heating <- Best.Western.Premium.Schwarzwald.1..Halbjahr.2019.Zusammenfassung
 
@@ -69,13 +72,70 @@ IWT$Zeit <- fastPOSIXct(IWT$X, required.components = 5L)
 
 #Merge Heating data and IWT -> key = timestamp
 merged <- merge(bw_heating_sample_spread, IWT, by = "Zeit")
+names(merged)[names(merged)=="T"] <- "ActT"
 summary(merged)
+
 
 
 #linear model (basic)
 
 lm1 <- lm(Verbraucher ~ Occ, data = merged)
 summary(lm1)
+
+lm2 <- lm(Verbraucher ~ Occ + ActT + Td + Val + Win + Room, data = merged)
+summary(lm2)
+
+#enhance dataset
+
+merged$TempDelta <- merged$ActT - merged$Td
+#merged$Time <- NULL
+merged$Date <- fastDate(substr(merged$Zeit, 0, 10))
+#merged$Time <- substr(merged$Zeit, 12, 19)
+#merged$Time <- strptime(merged$Time, format = "%H:%M:%S")
+
+merged$weekday <- as.factor(weekdays(merged$Date))
+
+merged[merged$weekday == "Samstag" | merged$weekday ==  "Sonntag", "weekend"] <- 1
+merged[merged$weekday == "Montag" | merged$weekday ==  "Dienstag" | merged$weekday ==  "Mittwoch" | 
+         merged$weekday ==  "Donnerstag" | merged$weekday ==  "Freitag", "weekend"] <- 0
+  
+merged$weekend <- as.factor(merged$weekend)
+
+
+#add. lin. models
+
+lm3 <- lm(Verbraucher ~ TempDelta + Val, data = merged)
+summary(lm3)
+
+lm4 <- lm(Verbraucher ~ weekday, data = merged)
+summary(lm4)
+
+lm5 <- lm(Verbraucher ~ weekend, data = merged)
+summary(lm5)
+
+
+#Polynomial Regression Models
+
+merged <- na.omit(merged)
+
+poly_3 <- lm(Verbraucher ~ poly(as.numeric(TempDelta), 3), data = merged)
+summary(poly_3)
+#plot(poly_3)
+ggplot(merged) + 
+  geom_point(aes(Verbraucher, TempDelta, col = "Original")) +
+  stat_smooth(method = "lm", formula = y~poly(x,3), aes(TempDelta, poly_3$fitted.values, col = "Order 3"))
+
+
+###
+
+plot(merged$Date, merged$Verbraucher)
+plot(merged$Verbraucher, merged$Occ)
+plot(merged$Verbraucher, merged$weekday)
+plot(merged$Verbraucher, merged$weekend)
+plot(merged$Verbraucher, merged$TempDelta)
+plot(merged$Verbraucher, merged$Val)
+
+
 
 
 #####
